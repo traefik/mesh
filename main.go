c546e1f9b9e16d43376b13ec3cc5237efa15d714
Daniel Tomcej <daniel.tomcej@gmail.com>
2019-05-06 14:22:12 -0600

    cycle coredns pods

--- main.go
+++ main.go
@@ -1,26 +1,14 @@
-/*
-Copyright 2017 The Kubernetes Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Note: the example only works with the code within the same release/branch.
 package main
 
 import (
 	"flag"
 	"fmt"
 	"path/filepath"
+	"time"
 
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -49,46 +37,122 @@ func main() {
 		panic(err)
 	}
 
-	podsClient := clientset.CoreV1().Pods(apiv1.NamespaceDefault)
+	fmt.Println("Verifying mesh namespace exists...")
+	if err := verifyNamespaceExists(clientset, meshNamespace); err != nil {
+		panic(err)
+	}
 
-	fmt.Printf("Listing pods in namespace %q:\n", apiv1.NamespaceDefault)
-	podList, err := podsClient.List(metav1.ListOptions{})
+	fmt.Println("Listing services in all namespaces:")
+	serviceListAll, err := clientset.CoreV1().Services(apiv1.NamespaceAll).List(metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
-	for _, p := range podList.Items {
-		fmt.Printf(" * %s \n", p.Name)
+
+	for _, s := range serviceListAll.Items {
+		if s.Namespace == meshNamespace {
+			continue
+		}
+
+		fmt.Printf(" * %s/%s \n", s.Namespace, s.Name)
+
+		if err := verifyServiceExists(clientset, s.Namespace, s.Name); err != nil {
+			panic(err)
+		}
 	}
 
-	serviceClient := clientset.CoreV1().Services(apiv1.NamespaceAll)
-
-	fmt.Printf("Listing services in namespace %q:\n", apiv1.NamespaceAll)
-	serviceList, err := serviceClient.List(metav1.ListOptions{})
+	fmt.Printf("Listing services in mesh namespace %q:\n", meshNamespace)
+	serviceListMesh, err := clientset.CoreV1().Services(meshNamespace).List(metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
-	for _, s := range serviceList.Items {
+	for _, s := range serviceListMesh.Items {
 		fmt.Printf(" * %s/%s \n", s.Namespace, s.Name)
 	}
 
-	fmt.Printf("Listing pods in namespace %q:\n", meshNamespace)
-	podsClient = clientset.CoreV1().Pods(meshNamespace)
-	podList, err = podsClient.List(metav1.ListOptions{})
-	if err != nil {
+	fmt.Println("Patching CoreDNS pods...")
+	if err := patchCoreDNS(clientset, "coredns", "kube-system"); err != nil {
 		panic(err)
 	}
-	for _, p := range podList.Items {
-		fmt.Printf(" * %s \n", p.Name)
-	}
+}
 
-	fmt.Printf("Listing services in namespace %q:\n", meshNamespace)
-	serviceClient = clientset.CoreV1().Services(meshNamespace)
-	serviceList, err = serviceClient.List(metav1.ListOptions{})
+func verifyNamespaceExists(client *kubernetes.Clientset, namespace string) error {
+	_, err := client.CoreV1().Namespaces().Get(meshNamespace, metav1.GetOptions{})
 	if err != nil {
-		panic(err)
+		ns := &apiv1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: meshNamespace,
+			},
+			Spec: apiv1.NamespaceSpec{},
+		}
+
+		_, err := client.CoreV1().Namespaces().Create(ns)
+		if err != nil {
+			return err
+		}
+
 	}
-	for _, s := range serviceList.Items {
-		fmt.Printf(" * %s/%s \n", s.Namespace, s.Name)
+	return nil
+}
+
+func verifyServiceExists(client *kubernetes.Clientset, name, namespace string) error {
+	meshServiceName := fmt.Sprintf("%s-%s-%s", meshPodPrefix, namespace, name)
+	meshServiceInstance, err := client.CoreV1().Services(meshNamespace).Get(meshServiceName, metav1.GetOptions{})
+	if meshServiceInstance == nil || err != nil {
+		svc := &apiv1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      meshServiceName,
+				Namespace: meshNamespace,
+			},
+			Spec: apiv1.ServiceSpec{
+				Ports: []apiv1.ServicePort{
+					{
+						Port: 80,
+					},
+				},
+				Selector: map[string]string{
+					"mesh": "traefik-mesh",
+				},
+			},
+		}
+
+		_, err := client.CoreV1().Services(meshNamespace).Create(svc)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func patchCoreDNS(client *kubernetes.Clientset, deploymentName, deploymentNamespace string) error {
+	coreDeployment, err := client.AppsV1().Deployments(deploymentNamespace).Get(deploymentName, metav1.GetOptions{})
+	if err != nil {
+		return err
 	}
 
+	coreLabelSelector := labels.Set(coreDeployment.Spec.Selector.MatchLabels).String()
+
+	fmt.Printf("CoreDNS Selector: %v\n", coreLabelSelector)
+
+	corePods, err := client.CoreV1().Pods(deploymentNamespace).List(metav1.ListOptions{LabelSelector: coreLabelSelector})
+	if err != nil {
+		return err
+	}
+	for _, p := range corePods.Items {
+		fmt.Printf("Deleting pod %s...\n", p.Name)
+		if err := client.CoreV1().Pods(deploymentNamespace).Delete(p.Name, nil); err != nil {
+			return err
+		}
+		time.Sleep(5 * time.Second)
+		for {
+			d, err := client.AppsV1().Deployments(deploymentNamespace).Get(deploymentName, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			if d.Status.ReadyReplicas == d.Status.Replicas {
+				break
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}
+	return nil
 }
