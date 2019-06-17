2d693bf786304a78a5e6136f7a030ddc28ec944d
Daniel Tomcej <daniel.tomcej@gmail.com>
2019-06-17 02:54:21 -0600

    Allow for multiport services (#50)

--- meshcontroller/handler.go
+++ meshcontroller/handler.go
@@ -2,6 +2,7 @@ package meshcontroller
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/containous/i3o/k8s"
 	traefikv1alpha1 "github.com/containous/traefik/pkg/provider/kubernetes/crd/traefik/v1alpha1"
@@ -40,42 +41,46 @@ func (h *Handler) Init() error {
 // ObjectCreated is called when an object is created.
 func (h *Handler) ObjectCreated(obj interface{}) {
 	// assert the type to an object to pull out relevant data
-	service := obj.(*corev1.Service)
-	if h.IgnoredNamespaces.Contains(service.Namespace) {
+	userService := obj.(*corev1.Service)
+	if h.IgnoredNamespaces.Contains(userService.Namespace) {
 		return
 	}
 
-	log.Debugf("MeshControllerHandler ObjectCreated with type: *corev1.Service: %s/%s", service.Namespace, service.Name)
+	log.Debugf("MeshControllerHandler ObjectCreated with type: *corev1.Service: %s/%s", userService.Namespace, userService.Name)
 
-	log.Debugf("Verifying associated mesh service for service: %s/%s", service.Namespace, service.Name)
-	if err := h.verifyMeshServiceExists(service); err != nil {
+	log.Debugf("Verifying associated mesh service for service: %s/%s", userService.Namespace, userService.Name)
+	createdService, err := h.verifyMeshServiceExists(userService)
+	if err != nil {
 		log.Errorf("Could not verify mesh service exists: %v", err)
 		return
 	}
 
-	log.Debugf("Verifying associated mesh ingressroute for service: %s/%s", service.Namespace, service.Name)
-	if err := h.verifyMeshIngressRouteExists(service); err != nil {
+	log.Debugf("Verifying associated mesh ingressroute for service: %s/%s", userService.Namespace, userService.Name)
+	if err := h.verifyMeshIngressRouteExists(userService, createdService); err != nil {
 		log.Errorf("Could not verify mesh ingressroute exists: %v", err)
 	}
 
 }
 
 // ObjectDeleted is called when an object is deleted.
-func (h *Handler) ObjectDeleted(obj interface{}) {
-	// assert the type to an object to pull out relevant data
-	service := obj.(*corev1.Service)
+func (h *Handler) ObjectDeleted(key string, obj interface{}) {
+	name, namespace := keyToNameAndNamespace(key)
+	log.Debugf("MeshControllerHandler.ObjectDeleted: %s", key)
 
-	if h.IgnoredNamespaces.Contains(service.Namespace) {
-		return
-	}
-	log.Debugln("MeshControllerHandler.ObjectDeleted")
-	if err := h.verifyMeshServiceDeleted(service); err != nil {
-		log.Errorf("Could not verify mesh service deleted: %v", err)
-		return
-	}
+	// assert the type to find out what was deleted
+	if _, ok := obj.(corev1.Service); ok {
+		// This is a service, process as a deleted service.
+		if h.IgnoredNamespaces.Contains(namespace) {
+			return
+		}
+		if err := h.verifyMeshServiceDeleted(name, namespace); err != nil {
+			log.Errorf("Could not verify mesh service deleted: %v", err)
+			return
+		}
 
-	if err := h.verifyMeshIngressRouteDeleted(service); err != nil {
-		log.Errorf("Could not verify mesh ingressroute deleted: %v", err)
+		if err := h.verifyMeshIngressRouteDeleted(name, namespace); err != nil {
+			log.Errorf("Could not verify mesh ingressroute deleted: %v", err)
+		}
 	}
 }
 
@@ -84,39 +89,42 @@ func (h *Handler) ObjectUpdated(objOld, objNew interface{}) {
 	log.Debugln("MeshControllerHandler.ObjectUpdated")
 }
 
-func (h *Handler) verifyMeshServiceExists(service *apiv1.Service) error {
+func (h *Handler) verifyMeshServiceExists(service *apiv1.Service) (*apiv1.Service, error) {
 	meshServiceName := serviceToMeshName(service.Name, service.Namespace)
 	meshServiceInstance, err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Get(meshServiceName, metav1.GetOptions{})
 	if meshServiceInstance == nil || err != nil {
+		// Mesh service does not exist.
+		var ports []apiv1.ServicePort
+
+		for id, sp := range service.Spec.Ports {
+			meshPort := apiv1.ServicePort{
+				Name:       sp.Name,
+				Port:       sp.Port,
+				TargetPort: intstr.FromInt(5000 + id),
+			}
+
+			ports = append(ports, meshPort)
+		}
+
 		svc := &apiv1.Service{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      meshServiceName,
 				Namespace: k8s.MeshNamespace,
 			},
 			Spec: apiv1.ServiceSpec{
-				Ports: []apiv1.ServicePort{
-					{
-						Name:       "web",
-						Port:       80,
-						TargetPort: intstr.FromInt(8000),
-					},
-				},
+				Ports: ports,
 				Selector: map[string]string{
-					"app": "traefik-mesh-node",
+					"component": "i3o-mesh",
 				},
 			},
 		}
-
-		if _, err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Create(svc); err != nil {
-			return err
-		}
+		return h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Create(svc)
 	}
-
-	return nil
+	return meshServiceInstance, nil
 }
 
-func (h *Handler) verifyMeshServiceDeleted(service *apiv1.Service) error {
-	meshServiceName := serviceToMeshName(service.Name, service.Namespace)
+func (h *Handler) verifyMeshServiceDeleted(serviceName, serviceNamespace string) error {
+	meshServiceName := serviceToMeshName(serviceName, serviceNamespace)
 	meshServiceInstance, err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Get(meshServiceName, metav1.GetOptions{})
 	if err != nil {
 		return err
@@ -127,43 +135,49 @@ func (h *Handler) verifyMeshServiceDeleted(service *apiv1.Service) error {
 		if err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Delete(meshServiceName, &metav1.DeleteOptions{}); err != nil {
 			return err
 		}
+		log.Debugf("Deleted service: %s/%s", k8s.MeshNamespace, meshServiceName)
 	}
 
 	return nil
 }
 
-func (h *Handler) verifyMeshIngressRouteExists(service *apiv1.Service) error {
-	meshIngressRouteName := serviceToMeshName(service.Name, service.Namespace)
-	matchRule := fmt.Sprintf("Host(`%s.%s.traefik.mesh`) || Host(`%s`)", service.Name, service.Namespace, service.Spec.ClusterIP)
+func (h *Handler) verifyMeshIngressRouteExists(userService *apiv1.Service, createdService *apiv1.Service) error {
+	meshIngressRouteName := serviceToMeshName(userService.Name, userService.Namespace)
+	matchRule := fmt.Sprintf("Host(`%s.%s.traefik.mesh`) || Host(`%s`)", userService.Name, userService.Namespace, userService.Spec.ClusterIP)
 	labels := map[string]string{
-		"i3o-mesh": "internal",
+		"i3o-mesh":     "internal",
+		"user-service": userService.Name,
 	}
 
-	meshIngressRouteInstance, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(service.Namespace).Get(meshIngressRouteName, metav1.GetOptions{})
-	if meshIngressRouteInstance == nil || err != nil {
+	for _, sp := range createdService.Spec.Ports {
 		ir := &traefikv1alpha1.IngressRoute{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      meshIngressRouteName,
-				Namespace: service.Namespace,
+				Name:      fmt.Sprintf("%s-%d", meshIngressRouteName, sp.TargetPort.IntVal),
+				Namespace: userService.Namespace,
 				Labels:    labels,
 			},
 			Spec: traefikv1alpha1.IngressRouteSpec{
+				EntryPoints: []string{fmt.Sprintf("ingress-%d", sp.TargetPort.IntVal)},
 				Routes: []traefikv1alpha1.Route{
 					{
 						Match: matchRule,
 						Kind:  "Rule",
 						Services: []traefikv1alpha1.Service{
 							{
-								Name: service.Name,
-								Port: service.Spec.Ports[0].Port,
+								Name: userService.Name,
+								Port: sp.Port,
 							},
 						},
 					},
 				},
 			},
 		}
-		if _, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(metav1.NamespaceDefault).Create(ir); err != nil {
-			return err
+
+		irInstance, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(ir.Namespace).Get(ir.Name, metav1.GetOptions{})
+		if irInstance == nil || err != nil {
+			if _, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(ir.Namespace).Create(ir); err != nil {
+				return err
+			}
 		}
 
 	}
@@ -171,18 +185,17 @@ func (h *Handler) verifyMeshIngressRouteExists(service *apiv1.Service) error {
 	return nil
 }
 
-func (h *Handler) verifyMeshIngressRouteDeleted(service *apiv1.Service) error {
-	meshIngressRouteName := serviceToMeshName(service.Name, service.Namespace)
-	meshIngressRouteInstance, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(service.Namespace).Get(meshIngressRouteName, metav1.GetOptions{})
+func (h *Handler) verifyMeshIngressRouteDeleted(serviceName, serviceNamespace string) error {
+	selector := fmt.Sprintf("user-service=%s", serviceName)
+	irs, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(serviceNamespace).List(metav1.ListOptions{LabelSelector: selector})
 	if err != nil {
 		return err
 	}
-
-	if meshIngressRouteInstance != nil {
-		// CRD exists, delete
-		if err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(service.Namespace).Delete(meshIngressRouteName, &metav1.DeleteOptions{}); err != nil {
+	for _, ir := range irs.Items {
+		if err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(ir.Namespace).Delete(ir.Name, &metav1.DeleteOptions{}); err != nil {
 			return err
 		}
+		log.Debugf("Deleted IngressRoute: %s/%s", ir.Namespace, ir.Name)
 	}
 
 	return nil
@@ -191,4 +204,15 @@ func (h *Handler) verifyMeshIngressRouteDeleted(service *apiv1.Service) error {
 // serviceToMeshName converts a service with a namespace to a traefik-mesh ingressroute name.
 func serviceToMeshName(serviceName string, namespace string) string {
 	return fmt.Sprintf("traefik-%s-%s", namespace, serviceName)
+}
+
+// keyToNameAndNamespace splits a key to key and namespace strings
+func keyToNameAndNamespace(key string) (name, namespace string) {
+	splitKey := strings.Split(key, "/")
+	if len(splitKey) == 1 {
+		// No namespace in the key, return key in default namespace
+		return key, metav1.NamespaceDefault
+	}
+
+	return splitKey[1], splitKey[0]
 }
