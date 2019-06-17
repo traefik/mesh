713f5c817bf619ca3a5bdd57bfd0b30a755656e5
Michael <michael.matur@gmail.com>
2019-06-17 18:23:07 +0200

    Change api server port

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -22,7 +22,7 @@ import (
 var (
 	integration    = flag.Bool("integration", true, "run integration tests")
 	kubeConfigPath = "/tmp/k3s-output/kubeconfig.yaml"
-	masterURL      = "https://localhost:6443"
+	masterURL      = "https://localhost:8643"
 )
 
 func Test(t *testing.T) {
