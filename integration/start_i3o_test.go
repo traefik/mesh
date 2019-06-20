28b1409b81ab10a4552b061c8586cce320746289
Michael <michael.matur@gmail.com>
2019-06-20 08:06:34 +0200

    Simplify integration tests

--- integration/start_i3o_test.go
+++ integration/start_i3o_test.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"time"
 
-	traefikv1alpha1 "github.com/containous/traefik/pkg/provider/kubernetes/crd/traefik/v1alpha1"
+	"github.com/containous/i3o/integration/try"
 	"github.com/go-check/check"
 	checker "github.com/vdemeester/shakers"
 	corev1 "k8s.io/api/core/v1"
@@ -30,24 +30,13 @@ func (s *StartI3oSuite) TestSimpleStart(c *check.C) {
 	s.waitForI3oControllerStarted(c)
 	s.startWhoami(c)
 
-	var err error
-	var ingressRouteTCPList *traefikv1alpha1.IngressRouteTCPList
 	// Check that ingressroutetcps is created for the whoami service
-	ingressRouteTCPList, err = s.clients.CrdClient.TraefikV1alpha1().IngressRouteTCPs("whoami").List(v1.ListOptions{})
+	err := s.try.ListIngressRouteTCPs("whoami", 20*time.Second, try.HasIngressRouteTCPList(1), try.HasNamesIngressRouteTCPList(try.List{"whoami-whoami"}))
 	c.Assert(err, checker.IsNil)
-	c.Assert(ingressRouteTCPList, checker.NotNil)
-	c.Assert(len(ingressRouteTCPList.Items), checker.Equals, 1)
 
-	c.Assert(ingressRouteTCPList.Items[0].Name, checker.Contains, "whoami-whoami")
-
-	var ingressRouteList *traefikv1alpha1.IngressRouteList
 	// Check that ingressroutes is created for the whoami-http service
-	ingressRouteList, err = s.clients.CrdClient.TraefikV1alpha1().IngressRoutes("whoami").List(v1.ListOptions{})
+	err = s.try.ListIngressRoutes("whoami", 20*time.Second, try.HasIngressRouteList(1), try.HasNamesIngressRouteList(try.List{"whoami-whoami-http"}))
 	c.Assert(err, checker.IsNil)
-	c.Assert(ingressRouteList, checker.NotNil)
-	c.Assert(len(ingressRouteList.Items), checker.Equals, 1)
-
-	c.Assert(ingressRouteList.Items[0].Name, checker.Contains, "whoami-whoami-http")
 
 	// Get the whoami service in whoami namespace
 	service, err := s.clients.KubeClient.CoreV1().Services("whoami").Get("whoami", v1.GetOptions{})
@@ -58,13 +47,7 @@ func (s *StartI3oSuite) TestSimpleStart(c *check.C) {
 	_, err = s.clients.KubeClient.CoreV1().Services("whoami").Update(service)
 	c.Assert(err, checker.IsNil)
 
-	// FIXME remove
-	time.Sleep(10 * time.Second)
 	// Check that ingressroutetcs is updates for the whoami service.
-	ingressRouteTCPList, err = s.clients.CrdClient.TraefikV1alpha1().IngressRouteTCPs("whoami").List(v1.ListOptions{})
+	err = s.try.ListIngressRouteTCPs("whoami", 60*time.Second, try.HasIngressRouteTCPList(2), try.HasNamesIngressRouteTCPList(try.List{"whoami-whoami-5000", "whoami-whoami-5001"}))
 	c.Assert(err, checker.IsNil)
-	c.Assert(ingressRouteTCPList, checker.NotNil)
-	c.Assert(len(ingressRouteTCPList.Items), checker.Equals, 2)
-
-	c.Assert(ingressRouteTCPList.Items[0].Name, checker.Contains, "whoami-whoami")
 }
