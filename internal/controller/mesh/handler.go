bb04c54551a28c67c5ce511d4ee1c5d7bfb86356
Daniel Tomcej <daniel.tomcej@gmail.com>
2019-06-24 14:29:40 -0600

    add trafficTarget begining

--- internal/controller/mesh/handler.go
+++ internal/controller/mesh/handler.go
@@ -6,7 +6,11 @@ import (
 
 	"github.com/containous/i3o/internal/controller/i3o"
 	"github.com/containous/i3o/internal/k8s"
+	traefikconfig "github.com/containous/traefik/pkg/config"
 	traefikv1alpha1 "github.com/containous/traefik/pkg/provider/kubernetes/crd/traefik/v1alpha1"
+	smiAccessv1alpha1 "github.com/deislabs/smi-sdk-go/pkg/apis/access/v1alpha1"
+	// smiSpecsv1alpha1 "github.com/deislabs/smi-sdk-go/pkg/apis/specs/v1alpha1"
+	// smiSplitv1alpha1 "github.com/deislabs/smi-sdk-go/pkg/apis/split/v1alpha1"
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -42,39 +46,60 @@ func (h *Handler) Init() error {
 // ObjectCreated is called when an object is created.
 func (h *Handler) ObjectCreated(event i3o.Message) {
 	// assert the type to an object to pull out relevant data
-	userService := event.Object.(*corev1.Service)
-	if h.Ignored.Namespaces.Contains(userService.Namespace) {
-		return
-	}
+	switch obj := event.Object.(type) {
 
-	if h.Ignored.Services.Contains(userService.Name, userService.Namespace) {
-		return
-	}
-
-	log.Debugf("MeshControllerHandler ObjectCreated with type: *corev1.Service: %s/%s", userService.Namespace, userService.Name)
-
-	log.Debugf("Creating associated mesh service for service: %s/%s", userService.Namespace, userService.Name)
-	createdService, err := h.verifyMeshServiceExists(userService)
-	if err != nil {
-		log.Errorf("Could not create mesh service: %v", err)
-		return
-	}
-
-	if serviceType, ok := userService.Annotations[k8s.AnnotationServiceType]; ok {
-		if strings.ToLower(serviceType) == k8s.ServiceTypeHTTP {
-			// Use http ingressRoutes
-			log.Debugf("Creating associated mesh ingressroute for service: %s/%s", userService.Namespace, userService.Name)
-			if err := h.verifyMeshIngressRouteExists(userService, createdService); err != nil {
-				log.Errorf("Could not create mesh ingressroute: %v", err)
-			}
+	case *corev1.Service:
+		userService := obj
+		if h.Ignored.Namespaces.Contains(userService.Namespace) {
 			return
 		}
-	}
 
-	// Default to use ingressRouteTCP
-	log.Debugf("Creating associated mesh ingressrouteTCP for service: %s/%s", userService.Namespace, userService.Name)
-	if err := h.verifyMeshIngressRouteTCPExists(userService, createdService); err != nil {
-		log.Errorf("Could not create mesh ingressrouteTCP: %v", err)
+		if h.Ignored.Services.Contains(userService.Name, userService.Namespace) {
+			return
+		}
+
+		log.Debugf("MeshControllerHandler ObjectCreated with type: *corev1.Service: %s/%s", userService.Namespace, userService.Name)
+
+		log.Debugf("Creating associated mesh service for service: %s/%s", userService.Namespace, userService.Name)
+		createdService, err := h.createMeshService(userService)
+		if err != nil {
+			log.Errorf("Could not create mesh service: %v", err)
+			return
+		}
+
+		if serviceType, ok := userService.Annotations[k8s.AnnotationServiceType]; ok {
+			if strings.ToLower(serviceType) == k8s.ServiceTypeHTTP {
+				// Use http ingressRoutes
+				log.Debugf("Creating associated mesh ingressroute for service: %s/%s", userService.Namespace, userService.Name)
+				if err := h.createMeshIngressRoutes(userService, createdService); err != nil {
+					log.Errorf("Could not create mesh ingressroute: %v", err)
+				}
+				return
+			}
+		}
+
+		// Default to use ingressRouteTCP
+		log.Debugf("Creating associated mesh ingressrouteTCP for service: %s/%s", userService.Namespace, userService.Name)
+		if err := h.createMeshIngressRouteTCPs(userService, createdService); err != nil {
+			log.Errorf("Could not create mesh ingressrouteTCP: %v", err)
+		}
+
+	case *smiAccessv1alpha1.TrafficTarget:
+		trafficTarget := obj
+
+		if h.Ignored.Namespaces.Contains(trafficTarget.Namespace) {
+			return
+		}
+
+		log.Debugf("MeshControllerHandler ObjectCreated with type: *smiAccessv1alpha1.TrafficTarget: %s/%s", trafficTarget.Namespace, trafficTarget.Name)
+
+		if err := h.updateMeshServicesWithSMI(trafficTarget); err != nil {
+			log.Errorf("Could not update mesh services with smi: %v", err)
+		}
+
+		if err := h.createUpdateShadowServicesWithSMI(trafficTarget); err != nil {
+			log.Errorf("Could not update shadow services with smi: %v", err)
+		}
 	}
 
 }
@@ -93,7 +118,7 @@ func (h *Handler) ObjectDeleted(event i3o.Message) {
 
 	log.Debugf("MeshControllerHandler ObjectDeleted with type: *corev1.Service: %s/%s", userService.Namespace, userService.Name)
 
-	if err := h.verifyMeshServiceDeleted(userService.Name, userService.Namespace); err != nil {
+	if err := h.deleteMeshService(userService.Name, userService.Namespace); err != nil {
 		log.Errorf("Could not verify mesh service deleted: %v", err)
 		return
 	}
@@ -102,7 +127,7 @@ func (h *Handler) ObjectDeleted(event i3o.Message) {
 	if strings.ToLower(serviceType) == k8s.ServiceTypeHTTP {
 		// Use http ingressRoutes
 		log.Debugf("Deleting associated mesh ingressroute for service: %s/%s", userService.Namespace, userService.Name)
-		if err := h.verifyMeshIngressRouteDeleted(userService.Name, userService.Namespace); err != nil {
+		if err := h.deleteMeshIngressRoutesByService(userService.Name, userService.Namespace); err != nil {
 			log.Errorf("Could not delete mesh ingressroute: %v", err)
 		}
 		return
@@ -110,7 +135,7 @@ func (h *Handler) ObjectDeleted(event i3o.Message) {
 
 	// Default to use ingressRouteTCP
 	log.Debugf("Deleting associated mesh ingressrouteTCP for service: %s/%s", userService.Namespace, userService.Name)
-	if err := h.verifyMeshIngressRouteTCPDeleted(userService.Name, userService.Namespace); err != nil {
+	if err := h.deleteMeshIngressRouteTCPsByService(userService.Name, userService.Namespace); err != nil {
 		log.Errorf("Could not delete mesh ingressroute: %v", err)
 	}
 }
@@ -142,13 +167,13 @@ func (h *Handler) ObjectUpdated(event i3o.Message) {
 	if strings.ToLower(serviceType) == k8s.ServiceTypeHTTP {
 		// Use http ingressRoutes
 		log.Debugf("Deleting associated mesh ingressroute for service: %s/%s", oldService.Namespace, oldService.Name)
-		if err := h.verifyMeshIngressRouteDeleted(oldService.Name, oldService.Namespace); err != nil {
+		if err := h.deleteMeshIngressRoutesByService(oldService.Name, oldService.Namespace); err != nil {
 			log.Errorf("Could not delete mesh ingressroute: %v", err)
 		}
 	} else {
 		// Default to use ingressRouteTCP
 		log.Debugf("Deleting associated mesh ingressrouteTCP for service: %s/%s", oldService.Namespace, oldService.Name)
-		if err := h.verifyMeshIngressRouteTCPDeleted(oldService.Name, oldService.Namespace); err != nil {
+		if err := h.deleteMeshIngressRouteTCPsByService(oldService.Name, oldService.Namespace); err != nil {
 			log.Errorf("Could not delete mesh ingressroute: %v", err)
 		}
 	}
@@ -158,19 +183,19 @@ func (h *Handler) ObjectUpdated(event i3o.Message) {
 	if strings.ToLower(serviceType) == k8s.ServiceTypeHTTP {
 		// Use http ingressRoutes
 		log.Debugf("Creating associated mesh ingressroute for service: %s/%s", newService.Namespace, newService.Name)
-		if err := h.verifyMeshIngressRouteExists(newService, updatedMeshService); err != nil {
+		if err := h.createMeshIngressRoutes(newService, updatedMeshService); err != nil {
 			log.Errorf("Could not crea mesh ingressroute: %v", err)
 		}
 	} else {
 		// Default to use ingressRouteTCP
 		log.Debugf("Creating associated mesh ingressrouteTCP for service: %s/%s", newService.Namespace, newService.Name)
-		if err := h.verifyMeshIngressRouteTCPExists(newService, updatedMeshService); err != nil {
+		if err := h.createMeshIngressRouteTCPs(newService, updatedMeshService); err != nil {
 			log.Errorf("Could not create mesh ingressrouteTCP: %v", err)
 		}
 	}
 }
 
-func (h *Handler) verifyMeshServiceExists(service *corev1.Service) (*corev1.Service, error) {
+func (h *Handler) createMeshService(service *corev1.Service) (*corev1.Service, error) {
 	meshServiceName := userServiceToMeshServiceName(service.Name, service.Namespace)
 	meshServiceInstance, err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Get(meshServiceName, metav1.GetOptions{})
 	if meshServiceInstance == nil || err != nil {
@@ -211,7 +236,7 @@ func (h *Handler) verifyMeshServiceExists(service *corev1.Service) (*corev1.Serv
 	return meshServiceInstance, nil
 }
 
-func (h *Handler) verifyMeshServiceDeleted(serviceName, serviceNamespace string) error {
+func (h *Handler) deleteMeshService(serviceName, serviceNamespace string) error {
 	meshServiceName := userServiceToMeshServiceName(serviceName, serviceNamespace)
 	meshServiceInstance, err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Get(meshServiceName, metav1.GetOptions{})
 	if err != nil {
@@ -237,12 +262,13 @@ func (h *Handler) updateMeshService(oldUserService *corev1.Service, newUserServi
 
 	var updatedSvc *corev1.Service
 	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		existing, err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Get(meshServiceName, metav1.GetOptions{})
+		svc, err := h.Clients.KubeClient.CoreV1().Services(k8s.MeshNamespace).Get(meshServiceName, metav1.GetOptions{})
 		if err != nil {
 			return err
 		}
-
-		if existing != nil {
+		if svc != nil {
+			// Never modify the original object, always use a copy
+			existing := svc.DeepCopy()
 			var ports []corev1.ServicePort
 
 			for id, sp := range newUserService.Spec.Ports {
@@ -280,7 +306,7 @@ func (h *Handler) updateMeshService(oldUserService *corev1.Service, newUserServi
 
 }
 
-func (h *Handler) verifyMeshIngressRouteExists(userService *corev1.Service, createdService *corev1.Service) error {
+func (h *Handler) createMeshIngressRoutes(userService *corev1.Service, createdService *corev1.Service) error {
 	meshIngressRouteName := userServiceToMeshServiceName(userService.Name, userService.Namespace)
 	matchRule := fmt.Sprintf("Host(`%s.%s.traefik.mesh`) || Host(`%s`)", userService.Name, userService.Namespace, userService.Spec.ClusterIP)
 	labels := map[string]string{
@@ -324,7 +350,7 @@ func (h *Handler) verifyMeshIngressRouteExists(userService *corev1.Service, crea
 	return nil
 }
 
-func (h *Handler) verifyMeshIngressRouteTCPExists(userService *corev1.Service, createdService *corev1.Service) error {
+func (h *Handler) createMeshIngressRouteTCPs(userService *corev1.Service, createdService *corev1.Service) error {
 	meshIngressRouteName := userServiceToMeshServiceName(userService.Name, userService.Namespace)
 	matchRule := fmt.Sprintf("HostSNI(`%s.%s.traefik.mesh`) || HostSNI(`%s`)", userService.Name, userService.Namespace, userService.Spec.ClusterIP)
 	labels := map[string]string{
@@ -367,7 +393,7 @@ func (h *Handler) verifyMeshIngressRouteTCPExists(userService *corev1.Service, c
 	return nil
 }
 
-func (h *Handler) verifyMeshIngressRouteDeleted(serviceName, serviceNamespace string) error {
+func (h *Handler) deleteMeshIngressRoutesByService(serviceName, serviceNamespace string) error {
 	selector := fmt.Sprintf("user-service=%s", serviceName)
 	irs, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRoutes(serviceNamespace).List(metav1.ListOptions{LabelSelector: selector})
 	if err != nil {
@@ -383,7 +409,7 @@ func (h *Handler) verifyMeshIngressRouteDeleted(serviceName, serviceNamespace st
 	return nil
 }
 
-func (h *Handler) verifyMeshIngressRouteTCPDeleted(serviceName, serviceNamespace string) error {
+func (h *Handler) deleteMeshIngressRouteTCPsByService(serviceName, serviceNamespace string) error {
 	selector := fmt.Sprintf("user-service=%s", serviceName)
 	irtcps, err := h.Clients.CrdClient.TraefikV1alpha1().IngressRouteTCPs(serviceNamespace).List(metav1.ListOptions{LabelSelector: selector})
 	if err != nil {
@@ -396,6 +422,79 @@ func (h *Handler) verifyMeshIngressRouteTCPDeleted(serviceName, serviceNamespace
 		log.Debugf("Deleted IngressRouteTCP: %s/%s", irtcp.Namespace, irtcp.Name)
 	}
 
+	return nil
+}
+
+func (h *Handler) updateMeshServicesWithSMI(obj interface{}) error {
+	trafficTarget := obj.(*smiAccessv1alpha1.TrafficTarget)
+
+	var sourceIPs []string
+	for _, source := range trafficTarget.Sources {
+		fieldSelector := fmt.Sprintf("spec.serviceAccountName=%s", source.Name)
+		// Get all pods with the associated source serviceAccount.
+		pods, err := h.Clients.KubeClient.CoreV1().Pods(metav1.NamespaceAll).List(metav1.ListOptions{FieldSelector: fieldSelector})
+		if err != nil {
+			return err
+		}
+
+		// Retrieve a list of sourceIPs from the list of pods.
+		for _, pod := range pods.Items {
+			sourceIPs = append(sourceIPs, pod.Status.PodIP)
+		}
+	}
+
+	h.createUpdateIPWhitelistMiddleware(trafficTarget.Name, trafficTarget.Namespace, sourceIPs)
+
+	return nil
+}
+
+func (h *Handler) createUpdateIPWhitelistMiddleware(name string, namespace string, ips []string) error {
+
+	ipWhitelistMiddlewareName := name + namespace + "-whitelist"
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		mw, err := h.Clients.CrdClient.TraefikV1alpha1().Middlewares(k8s.MeshNamespace).Get(ipWhitelistMiddlewareName, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		if mw != nil {
+			// Never modify the original object, always use a copy
+			// Update middleware, using a copy
+			middleware := mw.DeepCopy()
+			if middleware.Spec.IPWhiteList == nil {
+				return fmt.Errorf("Could not update mesh whitelist: %s/%s is not type whitelist", middleware.Namespace, middleware.Name)
+			}
+
+			middleware.Spec.IPWhiteList.SourceRange = ips
+
+			_, err = h.Clients.CrdClient.TraefikV1alpha1().Middlewares(k8s.MeshNamespace).Update(middleware)
+			return err
+		}
+
+		// Create middleware.
+		middleware := &traefikv1alpha1.Middleware{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      ipWhitelistMiddlewareName,
+				Namespace: k8s.MeshNamespace,
+			},
+			Spec: traefikconfig.Middleware{
+				IPWhiteList: &traefikconfig.IPWhiteList{
+					SourceRange: ips,
+				},
+			},
+		}
+
+		_, err = h.Clients.CrdClient.TraefikV1alpha1().Middlewares(k8s.MeshNamespace).Create(middleware)
+		return err
+	})
+
+	if retryErr != nil {
+		return fmt.Errorf("unable to update middleware %q: %v", ipWhitelistMiddlewareName, retryErr)
+	}
+
+	return nil
+}
+
+func (h *Handler) createUpdateShadowServicesWithSMI(obj interface{}) error {
 	return nil
 }
 
