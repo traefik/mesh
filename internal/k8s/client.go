a4c1ff7c083b790151ba1cd7f36221329d8346a3
Michael <michael.matur@gmail.com>
2019-07-04 10:49:46 +0200

    fix after rebase

--- internal/k8s/client.go
+++ internal/k8s/client.go
@@ -502,7 +502,7 @@ func translateNotFoundError(err error) (bool, error) {
 
 // MustParseYaml parses a YAML to objects.
 func MustParseYaml(content []byte) []runtime.Object {
-	acceptedK8sTypes := regexp.MustCompile(`(Deployment|Endpoints|Service|Ingress|Middleware|Secret|TLSOption)`)
+	acceptedK8sTypes := regexp.MustCompile(`(Deployment|Endpoints|Service|Ingress|Middleware|Secret|TLSOption|Namespace)`)
 
 	files := strings.Split(string(content), "---")
 	retVal := make([]runtime.Object, 0, len(files))
