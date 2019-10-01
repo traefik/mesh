09fdd5fd4706d06efbf2f06eda5052770e5c99e8
Daniel Tomcej <daniel.tomcej@gmail.com>
2019-10-01 14:22:54 -0500

    remove extra err

--- integration/coredns_test.go
+++ integration/coredns_test.go
@@ -63,9 +63,8 @@ func (s *CoreDNSSuite) TestCoreDNSVersion(c *check.C) {
 		s.setCoreDNSVersion(c, test.version)
 		err := s.installHelmMaesh(c, false, false)
 		if test.expectedError {
+			err = s.waitForMaeshControllerStartedWithReturn(c)
 			c.Assert(err, checker.NotNil)
-			controllerErr := s.waitForMaeshControllerStartedWithReturn(c)
-			c.Assert(controllerErr, checker.NotNil)
 		} else {
 			c.Assert(err, checker.IsNil)
 			s.waitForMaeshControllerStarted(c)
