88c45bff1567c13a7f963218a6872312554db72f
Julien Levesy <jlevesy@gmail.com>
2019-11-15 04:55:56 +0100

    add mesher controller and tests

--- internal/mesher/controller.go
+++ internal/mesher/controller.go
@@ -2,9 +2,16 @@ package mesher
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	informersv1 "k8s.io/client-go/informers/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
 	clientv1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	listersv1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
@@ -16,6 +23,8 @@ type Filter interface {
 	Match(metav1.Object) bool
 }
 
+const moduleName = "mesher"
+
 // Controller is running the control loop to maintain the list of mesh services.
 type Controller struct {
 	svcCache  listersv1.ServiceLister
@@ -23,39 +32,74 @@ type Controller struct {
 
 	queue  workqueue.RateLimitingInterface
 	exited chan struct{}
+
+	currentNamespace string
+
+	logger logrus.FieldLogger
 }
 
 // NewController setup event handlers on the service informer and returns a controller.
-func NewController(client clientv1.ServiceInterface, informer informersv1.ServiceInformer, filter Filter) *Controller {
-	q := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "mesher")
+func NewController(clientSet kubernetes.Interface, informerFactory informers.SharedInformerFactory, ns string, l logrus.FieldLogger) *Controller {
+	q := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), moduleName)
 
-	informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) {},
-		UpdateFunc: func(oldObj, newObj interface{}) {},
-		DeleteFunc: func(obj interface{}) {},
+	svcInformer := informerFactory.Core().V1().Services()
+
+	svcInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(obj interface{}) { enqueueObj(q, l, typeAdd, obj) },
+		UpdateFunc: func(_, obj interface{}) { enqueueObj(q, l, typeUpdate, obj) },
+		DeleteFunc: func(obj interface{}) { enqueueObj(q, l, typeDelete, obj) },
 	})
 
 	return &Controller{
-		svcCache:  informer.Lister(),
-		svcClient: client,
-		queue:     q,
-		exited:    make(chan struct{}),
+		svcCache:         svcInformer.Lister(),
+		svcClient:        clientSet.CoreV1().Services(ns),
+		queue:            q,
+		exited:           make(chan struct{}),
+		currentNamespace: ns,
+		logger:           l.WithField("module", moduleName),
 	}
 }
 
-// Run runs the controller.
+// Run process all events coming from the workqueue until shutdown is signaled.
 func (c *Controller) Run() {
-	for {
-		evt, shutdown := c.queue.Get()
-		if shutdown {
-			close(c.exited)
-			return
-		}
+	defer close(c.exited)
 
-		defer c.queue.Done(evt)
-
-		c.queue.Forget(evt)
+	for c.processEvent() {
 	}
+}
+
+func (c *Controller) processEvent() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+
+	defer c.queue.Done(item)
+
+	evt := item.(event)
+
+	var err error
+
+	switch evt.Type {
+	case typeAdd:
+		err = c.createMeshService(evt.Key)
+	case typeUpdate:
+		err = c.updateMeshService(evt.Key)
+	case typeDelete:
+		err = c.deleteMeshService(evt.Key)
+	default:
+		c.logger.Errorf("Unknown event type %q, ignoring", evt.Type)
+		c.queue.Forget(item)
+		return true
+	}
+
+	if err != nil {
+		c.handleError(item, err)
+		return true
+	}
+
+	c.queue.Forget(item)
+	return true
 }
 
 // ShutDown shutdowns the controller.
@@ -70,4 +114,211 @@ func (c *Controller) Wait(ctx context.Context) error {
 	case <-c.exited:
 		return nil
 	}
+}
+
+const (
+	maxAttempts = 3
+)
+
+func (c *Controller) handleError(item interface{}, err error) {
+	l := c.logger.WithError(err)
+
+	attempt := c.queue.NumRequeues(item)
+	if attempt < maxAttempts {
+		l.Errorf("(%d/%d) unable to process item, retrying", attempt+1, maxAttempts)
+		c.queue.AddRateLimited(item)
+		return
+	}
+
+	l.Errorf("Max attempt reached, ignoring failing event")
+	c.queue.Forget(item)
+}
+
+func (c *Controller) createMeshService(key string) error {
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return fmt.Errorf("unable to parse key: %w", err)
+	}
+
+	addedSvc, err := c.svcCache.Services(ns).Get(name)
+	if err != nil {
+		return fmt.Errorf("unable to get added svc from cache: %v", err)
+	}
+
+	meshSvcName := meshServiceName(name, ns, c.currentNamespace)
+
+	_, err = c.svcCache.Services(c.currentNamespace).Get(meshSvcName)
+	if err == nil {
+		// If there is a maesh service already present, then we try to update it.
+		return c.updateMeshService(key)
+	}
+	if !errors.IsNotFound(err) {
+		return fmt.Errorf("unable to lookup for maesh service: %w", err)
+	}
+
+	_, err = c.svcClient.Create(c.buildMeshSvc(meshSvcName, addedSvc))
+	if err != nil {
+		return fmt.Errorf("unable to create mesh service: %v", err)
+	}
+
+	c.logger.Infof(
+		"Created mesh service %q, from created service %q from namespace %q",
+		meshSvcName,
+		name,
+		ns,
+	)
+
+	return nil
+}
+
+func (c *Controller) updateMeshService(key string) error {
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return fmt.Errorf("unable to parse key: %w", err)
+	}
+
+	updatedService, err := c.svcCache.Services(ns).Get(name)
+	if err != nil {
+		return fmt.Errorf("unable to get updated svc from cache: %v", err)
+	}
+
+	meshSvcName := meshServiceName(name, ns, c.currentNamespace)
+
+	_, err = c.svcClient.Update(c.buildMeshSvc(meshSvcName, updatedService))
+	if err != nil {
+		return fmt.Errorf("unable to update mesh service: %v", err)
+	}
+
+	c.logger.Infof(
+		"Updated mesh service %q, from an updated of service %q from namespace %q",
+		meshSvcName,
+		name,
+		ns,
+	)
+
+	return nil
+}
+
+func (c *Controller) deleteMeshService(key string) error {
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return fmt.Errorf("unable to parse key: %w", err)
+	}
+
+	meshSvcName := meshServiceName(name, ns, c.currentNamespace)
+
+	if err = c.svcClient.Delete(meshSvcName, nil); err != nil {
+		return fmt.Errorf("unable to delete mesh service: %v", err)
+	}
+
+	c.logger.Infof(
+		"Deleted mesh service %q, because of deletion of service %q from namespace %q",
+		meshSvcName,
+		name,
+		ns,
+	)
+
+	return nil
+}
+
+const (
+	portRangeStart = 5000
+
+	appLabel       = "app"
+	componentLabel = "component"
+
+	appLabelMaesh          = "maesh"
+	componentLabelMeshSvc  = "mesh-svc"
+	componentLabelMeshNode = "mesh-node"
+)
+
+func (c *Controller) buildMeshSvc(name string, sourceSvc *corev1.Service) *corev1.Service {
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: c.currentNamespace,
+			Labels: map[string]string{
+				appLabel:       appLabelMaesh,
+				componentLabel: componentLabelMeshSvc,
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				appLabel:       appLabelMaesh,
+				componentLabel: componentLabelMeshNode,
+			},
+		},
+	}
+
+	for id, port := range sourceSvc.Spec.Ports {
+		port := corev1.ServicePort{
+			Name:       port.Name,
+			Protocol:   port.Protocol,
+			Port:       port.Port,
+			TargetPort: intstr.FromInt(portRangeStart + id),
+		}
+
+		svc.Spec.Ports = append(svc.Spec.Ports, port)
+	}
+
+	return svc
+}
+
+func meshServiceName(name, ns, currentNs string) string {
+	return fmt.Sprintf("%s-%s-%s", currentNs, name, ns)
+}
+
+func isMeshService(obj interface{}) (bool, error) {
+	objMeta, err := meta.Accessor(obj)
+	if err != nil {
+		return false, err
+	}
+
+	objLabels := objMeta.GetLabels()
+
+	isMaeshService := objLabels[appLabel] == appLabelMaesh
+	isMeshSvc := objLabels[componentLabel] == componentLabelMeshSvc
+
+	return isMaeshService && isMeshSvc, nil
+}
+
+func enqueueObj(q workqueue.RateLimitingInterface, log logrus.FieldLogger, t eventType, obj interface{}) {
+	isMeshSvc, err := isMeshService(obj)
+	if err != nil {
+		log.WithError(err).Error("unable to detect mesh service from event, ignoring")
+		return
+	}
+
+	if isMeshSvc {
+		return
+	}
+
+	var key string
+
+	if t == typeDelete {
+		key, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	} else {
+		key, err = cache.MetaNamespaceKeyFunc(obj)
+	}
+
+	if err != nil {
+		log.Errorf("ignoring item, unable to calculate meta namespace key: %v", err)
+		return
+	}
+
+	q.Add(event{Type: t, Key: key})
+}
+
+type eventType int8
+
+const (
+	typeUnknown eventType = iota
+	typeAdd
+	typeUpdate
+	typeDelete
+)
+
+type event struct {
+	Type eventType
+	Key  string
 }
--- internal/mesher/controller_test.go
+++ internal/mesher/controller_test.go
@@ -2,16 +2,177 @@ package mesher_test
 
 import (
 	"context"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/containous/maesh/internal/mesher"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes/fake"
 )
+
+const meshNamespace = "maesh"
+
+func TestController(t *testing.T) {
+	tests := []struct {
+		name            string
+		createResources func(*testing.T, *fake.Clientset)
+		updateResources func(*testing.T, *fake.Clientset)
+
+		wantService             *corev1.Service
+		wantMeshServiceDeletion bool
+	}{
+		{
+			name: "creates mesh service on add",
+			createResources: func(t *testing.T, c *fake.Clientset) {
+				t.Helper()
+				_, err := c.CoreV1().Services("app").Create(generateAppService(8080))
+				require.NoError(t, err)
+			},
+			wantService: generateMeshService(8080),
+		},
+		{
+			name: "update mesh service on add if it already exists",
+			createResources: func(t *testing.T, c *fake.Clientset) {
+				t.Helper()
+				_, err := c.CoreV1().Services("app").Create(generateAppService(8080))
+				require.NoError(t, err)
+
+				_, err = c.CoreV1().Services(meshNamespace).Create(generateMeshService(9090))
+				require.NoError(t, err)
+			},
+			wantService: generateMeshService(8080),
+		},
+		{
+			name: "updates mesh service",
+			createResources: func(t *testing.T, c *fake.Clientset) {
+				t.Helper()
+				_, err := c.CoreV1().Services("app").Create(generateAppService(9999))
+				require.NoError(t, err)
+			},
+			updateResources: func(t *testing.T, c *fake.Clientset) {
+				_, err := c.CoreV1().Services("app").Update(generateAppService(8080))
+				require.NoError(t, err)
+			},
+			wantService: generateMeshService(8080),
+		},
+		{
+			name: "deletes mesh service",
+			createResources: func(t *testing.T, c *fake.Clientset) {
+				t.Helper()
+				_, err := c.CoreV1().Services("app").Create(generateAppService(8080))
+				require.NoError(t, err)
+			},
+			updateResources: func(t *testing.T, c *fake.Clientset) {
+				t.Helper()
+				err := c.CoreV1().Services("app").Delete("foo", nil)
+				require.NoError(t, err)
+			},
+			wantMeshServiceDeletion: true,
+			wantService:             generateMeshService(8080),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			clientSet := fake.NewSimpleClientset()
+			factory := informers.NewSharedInformerFactory(clientSet, 0)
+
+			controller := mesher.NewController(clientSet, factory, meshNamespace, logger())
+
+			factory.Start(ctx.Done())
+
+			test.createResources(t, clientSet)
+			factory.WaitForCacheSync(ctx.Done())
+
+			go controller.Run()
+
+			// Forces the processing of the createResources events by the controller.
+			time.Sleep(time.Millisecond)
+
+			if test.updateResources != nil {
+				test.updateResources(t, clientSet)
+				factory.WaitForCacheSync(ctx.Done())
+			}
+
+			controller.ShutDown()
+			_ = controller.Wait(ctx)
+
+			if test.wantMeshServiceDeletion {
+				assertMeshServiceDeletion(t, clientSet, test.wantService)
+				return
+			}
+
+			assertHasService(t, clientSet, test.wantService)
+		})
+	}
+}
+
+func generateAppService(port int32) *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "app",
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       port,
+					TargetPort: intstr.FromInt(8080),
+				},
+			},
+
+			Selector: map[string]string{
+				"app": "foo",
+			},
+			Type: corev1.ServiceTypeClusterIP,
+		},
+	}
+}
+
+func generateMeshService(port int32) *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "maesh-foo-app",
+			Namespace: meshNamespace,
+			Labels: map[string]string{
+				"app":       "maesh",
+				"component": "mesh-svc",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       port,
+					TargetPort: intstr.FromInt(5000),
+				},
+			},
+			Selector: map[string]string{
+				"app":       "maesh",
+				"component": "mesh-node",
+			},
+		},
+	}
+}
+
+func updateRessourceDeleteService(t *testing.T, c *fake.Clientset) {
+	t.Helper()
+	err := c.CoreV1().Services("app").Delete("foo", &metav1.DeleteOptions{})
+	require.NoError(t, err)
+}
 
 func assertHasService(t *testing.T, c *fake.Clientset, want *corev1.Service) {
 	t.Helper()
@@ -23,64 +184,17 @@ func assertHasService(t *testing.T, c *fake.Clientset, want *corev1.Service) {
 	assert.Equal(t, want, got)
 }
 
-func TestController(t *testing.T) {
-	tests := []struct {
-		name            string
-		createResources func(*testing.T, *fake.Clientset)
+func assertMeshServiceDeletion(t *testing.T, c *fake.Clientset, svc *corev1.Service) {
+	t.Helper()
 
-		wantService *corev1.Service
-	}{
-		{
-			name: "creates mesh service",
-			createResources: func(t *testing.T, c *fake.Clientset) {
-				t.Helper()
-				c.CoreV1().Services("test").Create(&corev1.Service{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "svc",
-					},
-					Spec: corev1.ServiceSpec{
-						Ports: []corev1.ServicePort{
-							{
-								Name:       "http",
-								Protocol:   corev1.ProtocolTCP,
-								Port:       8080,
-								TargetPort: intstr.FromInt(8080),
-							},
-						},
-						Selector: map[string]string{
-							"app": "foo",
-						},
-						Type: corev1.ServiceTypeClusterIP,
-					},
-				})
-			},
-			wantService: &corev1.Service{},
-		},
-	}
+	_, err := c.CoreV1().Services(meshNamespace).Get(svc.GetName(), metav1.GetOptions{})
+	assert.Error(t, err)
+	assert.True(t, errors.IsNotFound(err))
+}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			ctx := context.Background()
-
-			clientSet := fake.NewSimpleClientset()
-			factory := informers.NewSharedInformerFactory(clientSet, 0)
-
-			factory.Start(ctx.Done())
-			factory.WaitForCacheSync(ctx.Done())
-
-			controller := mesher.NewController(
-				clientSet.CoreV1().Services(metav1.NamespaceAll),
-				factory.Core().V1().Services(),
-				nil, // TODO ?
-			)
-
-			go controller.Run()
-
-			test.createResources(t, clientSet)
-			controller.ShutDown()
-			controller.Wait(ctx)
-
-			assertHasService(t, clientSet, test.wantService)
-		})
-	}
+func logger() logrus.FieldLogger {
+	l := logrus.New()
+	l.SetOutput(os.Stdout)
+	l.SetLevel(logrus.InfoLevel)
+	return l
 }
