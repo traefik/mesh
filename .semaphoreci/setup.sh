563abbd02247894c63b83d9e188e4eaa9b61eefc
Daniel Tomcej <daniel.tomcej@gmail.com>
2020-01-13 08:13:29 -0600

    recursive copy

--- .semaphoreci/setup.sh
+++ .semaphoreci/setup.sh
@@ -23,7 +23,7 @@ if [ -f "./.semaphoreci/golang.sh" ]; then export GOROOT="/usr/local/golang/${GO
 if [ -f "./.semaphoreci/golang.sh" ]; then export GOTOOLDIR="/usr/local/golang/${GO_VERSION}/go/pkg/tool/linux_amd64"; fi
 go version
 mkdir -p /home/runner/src/github.com/containous
-cp /home/runner/maesh/ /home/runner/src/github.com/containous
+cp -r /home/runner/maesh /home/runner/src/github.com/containous
 cd /home/runner/src/github.com/containous/maesh
 if [ -f "./go.mod" ]; then export GO111MODULE=on; fi
 if [ -f "./go.mod" ]; then export GOPROXY=https://proxy.golang.org; fi
