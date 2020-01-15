cbaf72d45ea29acdc3782411c8ae86a5988098ea
Daniel Tomcej <daniel.tomcej@gmail.com>
2020-01-15 11:53:07 -0600

    remove duplicate namespace creation

--- integration/smi_test.go
+++ integration/smi_test.go
@@ -24,7 +24,6 @@ type SMISuite struct{ BaseSuite }
 func (s *SMISuite) SetUpSuite(c *check.C) {
 	s.startk3s(c)
 	s.startAndWaitForCoreDNS(c)
-	s.createRequiredNamespaces(c)
 }
 
 func (s *SMISuite) TearDownSuite(c *check.C) {
