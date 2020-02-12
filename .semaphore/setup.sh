370ecbb28a32e211ad4fadd07aa4bd8cea605f22
Daniel Tomcej <daniel.tomcej@gmail.com>
2020-02-12 12:35:56 -0600

    remove jank

--- .semaphore/setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /home/runner/src/github.com/containous
-cp -r /home/runner/maesh /home/runner/src/github.com/containous
-cd /home/runner/src/github.com/containous/maesh
-if [ -f "./go.mod" ]; then export GO111MODULE=on; fi
-if [ -f "./go.mod" ]; then export GOPROXY=https://proxy.golang.org; fi
-if [ -f "./go.mod" ]; then go mod download; fi
-df
