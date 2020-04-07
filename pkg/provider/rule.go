f36f3d6f8698c5b8d545f75957a67a6a745fda72
Harold Ozouf <harold.ozouf@gmail.com>
2020-04-07 11:28:17 +0200

    Add the new Provider
    This Provider is intended to replace the SMI and Kubernetes Providers.
    The goal of this provider is to support both SMI resources and Service annotations
    from the same Provider. This provider can work in two modes:
    - ACL on: All traffic is forbidden unless explicitly authorized via a TrafficTarget
    - ACL off: Every pods can hit every services, no restriction
    
    Notable improvements:
    - Ability to use service annotations as well as SMI resources in the same time.
    - Middleware support on TrafficSplit and TrafficTargets.
    
    Limitations:
    - Nested split are still not supported.

---
+++ pkg/provider/rule.go
@@ -0,0 +1,104 @@
+package provider
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/containous/maesh/pkg/topology"
+)
+
+func buildTrafficTargetRule(tt *topology.ServiceTrafficTarget) string {
+	var orRules []string
+
+	for _, spec := range tt.Specs {
+		for _, match := range spec.HTTPMatches {
+			var matchParts []string
+
+			// Handle Path filtering.
+			if match.PathRegex != "" {
+				pathRegex := match.PathRegex
+
+				if strings.HasPrefix(match.PathRegex, "/") {
+					pathRegex = strings.TrimPrefix(match.PathRegex, "/")
+				}
+
+				matchParts = append(matchParts, fmt.Sprintf("PathPrefix(`/{path:%s}`)", pathRegex))
+			}
+
+			// Handle Method filtering.
+			if len(match.Methods) > 0 {
+				var matchAll bool
+
+				for _, m := range match.Methods {
+					if m == "*" {
+						matchAll = true
+						break
+					}
+				}
+
+				if !matchAll {
+					methods := strings.Join(match.Methods, "`,`")
+					matchParts = append(matchParts, fmt.Sprintf("Method(`%s`)", methods))
+				}
+			}
+
+			// Conditions within a HTTPMatch must all be fulfilled to be considered valid.
+			if len(matchParts) > 0 {
+				matchCond := strings.Join(matchParts, " && ")
+				if len(matchParts) > 1 {
+					matchCond = fmt.Sprintf("(%s)", matchCond)
+				}
+
+				orRules = append(orRules, matchCond)
+			}
+		}
+	}
+
+	// At least one HTTPMatch in the Specs must be valid.
+	return strings.Join(orRules, " || ")
+}
+
+func buildHTTPRuleFromService(svc *topology.Service) string {
+	return fmt.Sprintf("Host(`%s.%s.maesh`) || Host(`%s`)", svc.Name, svc.Namespace, svc.ClusterIP)
+}
+
+func buildHTTPRuleFromTrafficTarget(tt *topology.ServiceTrafficTarget) string {
+	ttRule := buildTrafficTargetRule(tt)
+	httpRule := buildHTTPRuleFromService(tt.Service)
+
+	if ttRule != "" {
+		return fmt.Sprintf("(%s) && (%s)", httpRule, ttRule)
+	}
+
+	return httpRule
+}
+
+func buildHTTPRuleFromTrafficTargetIndirect(tt *topology.ServiceTrafficTarget) string {
+	ttRule := buildTrafficTargetRule(tt)
+	svcRule := buildHTTPRuleFromService(tt.Service)
+	indirectRule := "HeadersRegexp(`X-Forwarded-For`, `.+`)"
+
+	if ttRule != "" {
+		return fmt.Sprintf("(%s) && (%s) && %s", svcRule, ttRule, indirectRule)
+	}
+
+	return fmt.Sprintf("(%s) && %s", svcRule, indirectRule)
+}
+
+func buildHTTPRuleFromTrafficSplitIndirect(ts *topology.TrafficSplit) string {
+	svcRule := buildHTTPRuleFromService(ts.Service)
+	indirectRule := "HeadersRegexp(`X-Forwarded-For`, `.+`)"
+
+	return fmt.Sprintf("(%s) && %s", svcRule, indirectRule)
+}
+
+func buildTCPRouterRule() string {
+	return "HostSNI(`*`)"
+}
+
+func getRulePriority(rule string, priority int) int {
+	andOps := strings.Count(rule, "&&")
+	orOps := strings.Count(rule, "||")
+
+	return priority*1000 + (andOps + orOps)
+}
