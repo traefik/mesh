9d1426fc1ea380716edcc67fe8c0b8133aea5e65
Harold Ozouf <harold.ozouf@gmail.com>
2020-05-04 14:23:24 +0200

    refactor: Refactor middlewareBuilder to ease new middleware integration

--- pkg/annotations/middleware.go
+++ pkg/annotations/middleware.go
@@ -3,65 +3,102 @@ package annotations
 import (
 	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/containous/traefik/v2/pkg/config/dynamic"
 )
 
-const (
-	annotationRetryAttempts            = baseAnnotation + "retry-attempts"
-	annotationCircuitBreakerExpression = baseAnnotation + "circuit-breaker-expression"
-	annotationRateLimitAverage         = baseAnnotation + "ratelimit-average"
-	annotationRateLimitBurst           = baseAnnotation + "ratelimit-burst"
-)
+type middlewareBuilder func(middleware *dynamic.Middleware, annotations map[string]string) (bool, error)
 
 // BuildMiddleware builds a middleware from the given annotations
 func BuildMiddleware(annotations map[string]string) (*dynamic.Middleware, error) {
-	var middleware dynamic.Middleware
-
-	// Build circuit-breaker middleware.
-	if circuitBreakerExpression, ok := annotations[annotationCircuitBreakerExpression]; ok {
-		middleware.CircuitBreaker = &dynamic.CircuitBreaker{Expression: circuitBreakerExpression}
+	builders := []middlewareBuilder{
+		buildRetryMiddleware,
+		buildRateLimitMiddleware,
+		buildCircuitBreakerMiddleware,
 	}
 
-	// Build retry middleware.
-	if retryAttempts, ok := annotations[annotationRetryAttempts]; ok {
-		attempts, err := strconv.Atoi(retryAttempts)
+	var (
+		middleware        dynamic.Middleware
+		middlewareCounter int
+	)
+
+	for _, builder := range builders {
+		ok, err := builder(&middleware, annotations)
 		if err != nil {
-			return nil, fmt.Errorf("unable to build retry middleware, %q annotation is invalid: %w", annotationRetryAttempts, err)
+			return nil, err
 		}
 
-		middleware.Retry = &dynamic.Retry{Attempts: attempts}
-	}
-
-	// Build rate-limit middleware.
-	rateLimitAverage, hasRateLimitAverage := annotations[annotationRateLimitAverage]
-	rateLimitBurst, hasRateLimitBurst := annotations[annotationRateLimitBurst]
-
-	if hasRateLimitAverage && hasRateLimitBurst {
-		average, err := strconv.Atoi(rateLimitAverage)
-		if err != nil {
-			return nil, fmt.Errorf("unable to build rate-limit middleware, %q annotation is invalid: %w", annotationRateLimitAverage, err)
-		}
-
-		burst, err := strconv.Atoi(rateLimitBurst)
-		if err != nil {
-			return nil, fmt.Errorf("unable to build rate-limit middleware, %q annotation is invalid: %w", annotationRateLimitBurst, err)
-		}
-
-		if burst <= 0 || average <= 0 {
-			return nil, errors.New("unable to build rate-limit middleware, burst and average must be greater than 0")
-		}
-
-		middleware.RateLimit = &dynamic.RateLimit{
-			Average: int64(average),
-			Burst:   int64(burst),
+		if ok {
+			middlewareCounter++
 		}
 	}
 
-	if middleware.CircuitBreaker == nil && middleware.Retry == nil && middleware.RateLimit == nil {
+	if middlewareCounter == 0 {
 		return nil, nil
 	}
 
 	return &middleware, nil
+}
+
+func buildRetryMiddleware(middleware *dynamic.Middleware, annotations map[string]string) (bool, error) {
+	retryAttempts, errRetryAttempts := GetRetryAttempts(annotations)
+	if errRetryAttempts != nil {
+		if errRetryAttempts == ErrNotFound {
+			return false, nil
+		}
+
+		return false, fmt.Errorf("unable to build retry middleware: %w", errRetryAttempts)
+	}
+
+	middleware.Retry = &dynamic.Retry{Attempts: retryAttempts}
+
+	return true, nil
+}
+
+func buildRateLimitMiddleware(middleware *dynamic.Middleware, annotations map[string]string) (bool, error) {
+	var (
+		rateLimitBurst   int
+		rateLimitAverage int
+		err              error
+	)
+
+	rateLimitBurst, err = GetRateLimitBurst(annotations)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, fmt.Errorf("unable to build rate-limit middleware: %w", err)
+	}
+
+	rateLimitAverage, err = GetRateLimitAverage(annotations)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, fmt.Errorf("unable to build rate-limit middleware: %w", err)
+	}
+
+	if rateLimitBurst <= 0 || rateLimitAverage <= 0 {
+		return false, errors.New("unable to build rate-limit middleware: burst and average must be greater than 0")
+	}
+
+	middleware.RateLimit = &dynamic.RateLimit{
+		Burst:   int64(rateLimitBurst),
+		Average: int64(rateLimitAverage),
+	}
+
+	return true, nil
+}
+
+func buildCircuitBreakerMiddleware(middleware *dynamic.Middleware, annotations map[string]string) (bool, error) {
+	circuitBreakerExpression, errCircuitBreakerExpression := GetCircuitBreakerExpression(annotations)
+	if errCircuitBreakerExpression != nil {
+		if errCircuitBreakerExpression == ErrNotFound {
+			return false, nil
+		}
+
+		return false, fmt.Errorf("unable to build circuit-breaker middleware: %w", errCircuitBreakerExpression)
+	}
+
+	middleware.CircuitBreaker = &dynamic.CircuitBreaker{Expression: circuitBreakerExpression}
+
+	return true, nil
 }
