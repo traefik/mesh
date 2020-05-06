3a69a1ec991bb4629c8ca321c6d2e3d564e1051f
kevinpollet <pollet.kevin@gmail.com>
2020-05-06 09:42:19 +0400

    fix: rebuild config when a non ignored event is received

--- pkg/controller/handler.go
+++ pkg/controller/handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/containous/maesh/pkg/k8s"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 )
 
 // configMessageChanRebuild rebuild.
@@ -12,7 +13,7 @@ const configMessageChanRebuild = "rebuild"
 // Handler is an implementation of a ResourceEventHandler.
 type Handler struct {
 	log               logrus.FieldLogger
-	ignored           k8s.IgnoreWrapper
+	ignoredResources  k8s.IgnoreWrapper
 	configRefreshChan chan string
 	serviceManager    ServiceManager
 }
@@ -21,29 +22,22 @@ type Handler struct {
 func NewHandler(log logrus.FieldLogger, ignored k8s.IgnoreWrapper, serviceManager ServiceManager, configRefreshChan chan string) *Handler {
 	return &Handler{
 		log:               log,
-		ignored:           ignored,
+		ignoredResources:  ignored,
 		configRefreshChan: configRefreshChan,
 		serviceManager:    serviceManager,
 	}
 }
 
-// OnAdd executed when an object is added.
+// OnAdd is called when an object is added.
 func (h *Handler) OnAdd(obj interface{}) {
-	// assert the type to an object to pull out relevant data
-	switch obj := obj.(type) {
-	case *corev1.Service:
-		if h.ignored.IsIgnored(obj.ObjectMeta) {
-			return
-		}
+	if h.isIgnoredResource(obj) {
+		return
+	}
 
+	// If the created object is a service we should create a corresponding shadow service.
+	if obj, isService := obj.(*corev1.Service); isService {
 		if err := h.serviceManager.Create(obj); err != nil {
 			h.log.Errorf("Could not create mesh service: %v", err)
-		}
-	case *corev1.Endpoints:
-		return
-	case *corev1.Pod:
-		if !isMeshPod(obj) {
-			return
 		}
 	}
 
@@ -51,15 +45,29 @@ func (h *Handler) OnAdd(obj interface{}) {
 	h.configRefreshChan <- configMessageChanRebuild
 }
 
-// OnUpdate executed when an object is updated.
+// OnUpdate is called when an object is updated and ensures that the proper handler is called depending on the filter matches.
 func (h *Handler) OnUpdate(oldObj, newObj interface{}) {
-	// Assert the type to an object to pull out relevant data.
-	switch obj := newObj.(type) {
-	case *corev1.Service:
-		if h.ignored.IsIgnored(obj.ObjectMeta) {
-			return
-		}
+	isOldIgnored := h.isIgnoredResource(oldObj)
+	isNewIgnored := h.isIgnoredResource(newObj)
 
+	switch {
+	case isOldIgnored && isNewIgnored:
+		return
+
+	// Old object is not ignored anymore, so we can treat this as an add event.
+	case isOldIgnored && !isNewIgnored:
+		h.OnAdd(newObj)
+		return
+
+	// Old object is now ignored, so we can treat this as a delete event.
+	case !isOldIgnored && isNewIgnored:
+		h.OnDelete(oldObj)
+		return
+	}
+
+	// Old and new object are not ignored so this is a real update.
+	// If the updated object is a service we should update the corresponding shadow service.
+	if obj, isService := newObj.(*corev1.Service); isService {
 		oldSvc, ok := oldObj.(*corev1.Service)
 		if !ok {
 			h.log.Errorf("Old object is not a kubernetes Service")
@@ -71,51 +79,39 @@ func (h *Handler) OnUpdate(oldObj, newObj interface{}) {
 		}
 
 		h.log.Debugf("MeshControllerHandler ObjectUpdated with type: *corev1.Service: %s/%s", obj.Namespace, obj.Name)
-	case *corev1.Endpoints:
-		// We can use the same ignore for services and endpoints.
-		if h.ignored.IsIgnored(obj.ObjectMeta) {
-			return
-		}
-
-		h.log.Debugf("MeshControllerHandler ObjectUpdated with type: *corev1.Endpoints: %s/%s", obj.Namespace, obj.Name)
-	case *corev1.Pod:
-		if !isMeshPod(obj) {
-			// We don't track updates of user pods, updates are done through endpoints.
-			return
-		}
-
-		h.log.Debugf("MeshControllerHandler ObjectUpdated with type: *corev1.Pod: %s/%s", obj.Namespace, obj.Name)
 	}
 
 	// Trigger a configuration rebuild.
 	h.configRefreshChan <- configMessageChanRebuild
 }
 
-// OnDelete executed when an object is deleted.
+// OnDelete is called when an object is deleted.
 func (h *Handler) OnDelete(obj interface{}) {
-	// Assert the type to an object to pull out relevant data.
-	switch obj := obj.(type) {
-	case *corev1.Service:
-		if h.ignored.IsIgnored(obj.ObjectMeta) {
-			return
-		}
+	if h.isIgnoredResource(obj) {
+		return
+	}
 
+	// If the deleted object is a service we should delete the corresponding shadow service.
+	if obj, isService := obj.(*corev1.Service); isService {
 		h.log.Debugf("MeshControllerHandler ObjectDeleted with type: *corev1.Service: %s/%s", obj.Namespace, obj.Name)
 
 		if err := h.serviceManager.Delete(obj); err != nil {
 			h.log.Errorf("Could not delete mesh service: %v", err)
 		}
-	case *corev1.Endpoints:
-		// We can use the same ignore for services and endpoints.
-		if h.ignored.IsIgnored(obj.ObjectMeta) {
-			return
-		}
-
-		h.log.Debugf("MeshController ObjectDeleted with type: *corev1.Endpoints: %s/%s", obj.Namespace, obj.Name)
-	case *corev1.Pod:
-		return
 	}
 
 	// Trigger a configuration rebuild.
 	h.configRefreshChan <- configMessageChanRebuild
+}
+
+// isIgnoredResource returns true if the given resource should be ignored, false otherwise.
+func (h *Handler) isIgnoredResource(obj interface{}) bool {
+	accessor, err := meta.Accessor(obj)
+	if err != nil {
+		return true
+	}
+
+	pMeta := meta.AsPartialObjectMetadata(accessor)
+
+	return h.ignoredResources.IsIgnored(pMeta.ObjectMeta)
 }
