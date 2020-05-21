f82e59d07114198458fd7b410ea7d26b1810e8b6
kevinpollet <pollet.kevin@gmail.com>
2020-05-21 15:53:01 +0400

    refactor: rework controller loop to use a work queue

--- pkg/controller/handler.go
+++ pkg/controller/handler.go
@@ -3,71 +3,50 @@ package controller
 import (
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
 )
 
-// Handler is an implementation of a ResourceEventHandler.
-type Handler struct {
-	log               logrus.FieldLogger
-	configRefreshChan chan struct{}
-	serviceManager    ServiceManager
+type enqueueWorkHandler struct {
+	logger    logrus.FieldLogger
+	workQueue workqueue.RateLimitingInterface
 }
 
-// NewHandler creates a handler.
-func NewHandler(log logrus.FieldLogger, serviceManager ServiceManager, configRefreshChan chan struct{}) *Handler {
-	return &Handler{
-		log:               log,
-		configRefreshChan: configRefreshChan,
-		serviceManager:    serviceManager,
-	}
+// OnAdd is called when an object is added to the informers cache.
+func (h *enqueueWorkHandler) OnAdd(obj interface{}) {
+	h.enqueueWork(obj)
 }
 
-// OnAdd is called when an object is added.
-func (h *Handler) OnAdd(obj interface{}) {
-	// If the created object is a service we have to create a corresponding shadow service.
-	if obj, isService := obj.(*corev1.Service); isService {
-		h.log.Debugf("MeshControllerHandler ObjectAdded with type: *corev1.Service: %s/%s", obj.Namespace, obj.Name)
+// OnUpdate is called when an object is updated in the informers cache.
+func (h *enqueueWorkHandler) OnUpdate(oldObj interface{}, newObj interface{}) {
+	oldObjMeta, okOld := oldObj.(metav1.Object)
+	newObjMeta, okNew := newObj.(metav1.Object)
 
-		if err := h.serviceManager.Create(obj); err != nil {
-			h.log.Errorf("Could not create mesh service: %v", err)
-		}
+	// This is a resync event, no extra work is needed.
+	if okOld && okNew && oldObjMeta.GetResourceVersion() == newObjMeta.GetResourceVersion() {
+		return
 	}
 
-	// Trigger a configuration rebuild.
-	h.configRefreshChan <- struct{}{}
+	h.enqueueWork(newObj)
 }
 
-// OnUpdate is called when an object is updated.
-func (h *Handler) OnUpdate(oldObj, newObj interface{}) {
-	// If the updated object is a service we have to update the corresponding shadow service.
-	if obj, isService := newObj.(*corev1.Service); isService {
-		oldSvc, ok := oldObj.(*corev1.Service)
-		if !ok {
-			h.log.Errorf("Old object is not a kubernetes Service")
-			return
-		}
-
-		if _, err := h.serviceManager.Update(oldSvc, obj); err != nil {
-			h.log.Errorf("Could not update mesh service: %v", err)
-		}
-
-		h.log.Debugf("MeshControllerHandler ObjectUpdated with type: *corev1.Service: %s/%s", obj.Namespace, obj.Name)
-	}
-
-	// Trigger a configuration rebuild.
-	h.configRefreshChan <- struct{}{}
+// OnDelete is called when an object is removed from the informers cache.
+func (h *enqueueWorkHandler) OnDelete(obj interface{}) {
+	h.enqueueWork(obj)
 }
 
-// OnDelete is called when an object is deleted.
-func (h *Handler) OnDelete(obj interface{}) {
-	// If the deleted object is a service we have to delete the corresponding shadow service.
-	if obj, isService := obj.(*corev1.Service); isService {
-		h.log.Debugf("MeshControllerHandler ObjectDeleted with type: *corev1.Service: %s/%s", obj.Namespace, obj.Name)
-
-		if err := h.serviceManager.Delete(obj); err != nil {
-			h.log.Errorf("Could not delete mesh service: %v", err)
-		}
+func (h *enqueueWorkHandler) enqueueWork(obj interface{}) {
+	if _, isService := obj.(*corev1.Service); !isService {
+		h.workQueue.Add(configRefreshKey)
+		return
 	}
 
-	// Trigger a configuration rebuild.
-	h.configRefreshChan <- struct{}{}
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		h.logger.Errorf("Unable to create a work key for resource %#v", obj)
+		return
+	}
+
+	h.workQueue.Add(key)
 }
---
+++ pkg/controller/handler_test.go
@@ -0,0 +1,144 @@
+package controller
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/workqueue"
+)
+
+func TestEnqueueWorkHandler_OnAdd(t *testing.T) {
+	log := logrus.New()
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(logrus.DebugLevel)
+
+	workQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	handler := &enqueueWorkHandler{logger: log, workQueue: workQueue}
+	handler.OnAdd(&corev1.Pod{})
+
+	assert.Equal(t, 1, workQueue.Len())
+
+	currentKey, _ := workQueue.Get()
+
+	assert.Equal(t, configRefreshKey, currentKey)
+}
+
+func TestEnqueueWorkHandler_OnDelete(t *testing.T) {
+	log := logrus.New()
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(logrus.DebugLevel)
+
+	workQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	handler := &enqueueWorkHandler{logger: log, workQueue: workQueue}
+	handler.OnDelete(&corev1.Pod{})
+
+	assert.Equal(t, 1, workQueue.Len())
+
+	currentKey, _ := workQueue.Get()
+
+	assert.Equal(t, configRefreshKey, currentKey)
+}
+
+func TestEnqueueWorkHandler_OnUpdate(t *testing.T) {
+	tests := []struct {
+		desc        string
+		oldObj      interface{}
+		newObj      interface{}
+		expectedLen int
+	}{
+		{
+			desc: "should enqueue if this is not a re-sync event",
+			oldObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{ResourceVersion: "foo"},
+			},
+			newObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{ResourceVersion: "foo"},
+			},
+			expectedLen: 0,
+		},
+		{
+			desc: "should enqueue if this is a re-sync event",
+			oldObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{ResourceVersion: "foo"},
+			},
+			newObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{ResourceVersion: "bar"},
+			},
+			expectedLen: 1,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			log := logrus.New()
+
+			log.SetOutput(os.Stdout)
+			log.SetLevel(logrus.DebugLevel)
+
+			workQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+			handler := &enqueueWorkHandler{logger: log, workQueue: workQueue}
+			handler.OnUpdate(test.oldObj, test.newObj)
+
+			assert.Equal(t, test.expectedLen, workQueue.Len())
+		})
+	}
+}
+
+func TestEnqueueWorkHandler_enqueueWork(t *testing.T) {
+	tests := []struct {
+		desc        string
+		obj         interface{}
+		expectedLen int
+		expectedKey string
+	}{
+		{
+			desc:        "should enqueue a refresh key if obj is not a service",
+			obj:         &corev1.Endpoints{},
+			expectedLen: 1,
+			expectedKey: configRefreshKey,
+		},
+		{
+			desc: "should enqueue a meta namespace key if the obj is a service",
+			obj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "bar",
+				},
+			},
+			expectedLen: 1,
+			expectedKey: "bar/foo",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			log := logrus.New()
+
+			log.SetOutput(os.Stdout)
+			log.SetLevel(logrus.DebugLevel)
+
+			workQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+			handler := &enqueueWorkHandler{logger: log, workQueue: workQueue}
+			handler.enqueueWork(test.obj)
+
+			assert.Equal(t, test.expectedLen, workQueue.Len())
+
+			currentKey, _ := workQueue.Get()
+
+			fmt.Println(currentKey)
+
+			assert.Equal(t, test.expectedKey, currentKey)
+		})
+	}
+}
