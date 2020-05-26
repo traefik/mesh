dd511d93a6d8a4481e5fb4536d6fcdcd0d536d50
kevinpollet <pollet.kevin@gmail.com>
2020-05-26 16:05:39 +0400

    feat: retrieve port mappings from existing shadow services

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -75,8 +75,8 @@ type Controller struct {
 	shadowServiceManager *ShadowServiceManager
 	provider             *provider.Provider
 	ignoredResources     k8s.IgnoreWrapper
-	tcpStateTable        PortMapper
-	udpStateTable        PortMapper
+	tcpStateTable        *k8s.PortMapping
+	udpStateTable        *k8s.PortMapping
 	topologyBuilder      TopologyBuilder
 	currentConfiguration *safe.Safe
 	api                  api.Interface
@@ -96,8 +96,8 @@ type Controller struct {
 	trafficSplitLister   splitlister.TrafficSplitLister
 }
 
-// NewMeshController builds the informers and other required components of the mesh controller, and returns an
-// initialized mesh controller object.
+// NewMeshController builds the informers and other required components of the mesh controller, and returns an initialized
+// mesh controller object.
 func NewMeshController(clients k8s.Client, cfg Config, logger logrus.FieldLogger) (*Controller, error) {
 	ignoredResources := k8s.NewIgnored()
 
@@ -109,23 +109,11 @@ func NewMeshController(clients k8s.Client, cfg Config, logger logrus.FieldLogger
 	ignoredResources.AddIgnoredNamespace(metav1.NamespaceSystem)
 	ignoredResources.AddIgnoredApps("maesh", "jaeger")
 
-	tcpStateTable, err := k8s.NewPortMapping(clients.KubernetesClient(), cfg.Namespace, k8s.TCPStateConfigMapName, cfg.MinTCPPort, cfg.MaxTCPPort)
-	if err != nil {
-		return nil, err
-	}
-
-	udpStateTable, err := k8s.NewPortMapping(clients.KubernetesClient(), cfg.Namespace, k8s.UDPStateConfigMapName, cfg.MinUDPPort, cfg.MaxUDPPort)
-	if err != nil {
-		return nil, err
-	}
-
 	c := &Controller{
 		logger:           logger,
 		cfg:              cfg,
 		clients:          clients,
 		ignoredResources: ignoredResources,
-		tcpStateTable:    tcpStateTable,
-		udpStateTable:    udpStateTable,
 	}
 
 	c.init()
@@ -134,13 +122,6 @@ func NewMeshController(clients k8s.Client, cfg Config, logger logrus.FieldLogger
 }
 
 func (c *Controller) init() {
-	// Create SharedInformers for non-ACL related resources.
-	c.kubernetesFactory = informers.NewSharedInformerFactoryWithOptions(c.clients.KubernetesClient(), k8s.ResyncPeriod)
-	c.splitFactory = splitinformer.NewSharedInformerFactoryWithOptions(c.clients.SplitClient(), k8s.ResyncPeriod)
-
-	c.serviceLister = c.kubernetesFactory.Core().V1().Services().Lister()
-	c.shadowServiceManager = NewShadowServiceManager(c.logger, c.serviceLister, c.cfg.Namespace, c.tcpStateTable, c.udpStateTable, c.cfg.DefaultMode, c.cfg.MinHTTPPort, c.cfg.MaxHTTPPort, c.clients.KubernetesClient())
-
 	// Create the work queue and the enqueue handler.
 	c.workQueue = workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 	c.handler = cache.FilteringResourceEventHandler{
@@ -148,9 +129,13 @@ func (c *Controller) init() {
 		Handler:    &enqueueWorkHandler{logger: c.logger, workQueue: c.workQueue},
 	}
 
-	// Create listers and register the event handler to informers that are not ACL related.
+	// Create SharedInformers, listers and register the event handler to informers that are not ACL related.
+	c.kubernetesFactory = informers.NewSharedInformerFactoryWithOptions(c.clients.KubernetesClient(), k8s.ResyncPeriod)
+	c.splitFactory = splitinformer.NewSharedInformerFactoryWithOptions(c.clients.SplitClient(), k8s.ResyncPeriod)
+
 	c.podLister = c.kubernetesFactory.Core().V1().Pods().Lister()
 	c.endpointsLister = c.kubernetesFactory.Core().V1().Endpoints().Lister()
+	c.serviceLister = c.kubernetesFactory.Core().V1().Services().Lister()
 	c.trafficSplitLister = c.splitFactory.Split().V1alpha2().TrafficSplits().Lister()
 
 	c.kubernetesFactory.Core().V1().Services().Informer().AddEventHandler(c.handler)
@@ -175,6 +160,22 @@ func (c *Controller) init() {
 	c.currentConfiguration = safe.New(provider.NewDefaultDynamicConfig())
 
 	c.api = api.NewAPI(c.logger, c.cfg.APIPort, c.cfg.APIHost, c.currentConfiguration, c.podLister, c.cfg.Namespace)
+
+	c.tcpStateTable = NewPortMapping(c.cfg.Namespace, c.serviceLister, c.cfg.MinTCPPort, c.cfg.MaxTCPPort)
+
+	c.udpStateTable = NewPortMapping(c.cfg.Namespace, c.serviceLister, c.cfg.MinUDPPort, c.cfg.MaxUDPPort)
+
+	c.shadowServiceManager = NewShadowServiceManager(
+		c.logger,
+		c.serviceLister,
+		c.cfg.Namespace,
+		c.tcpStateTable,
+		c.udpStateTable,
+		c.cfg.DefaultMode,
+		c.cfg.MinHTTPPort,
+		c.cfg.MaxHTTPPort,
+		c.clients.KubernetesClient(),
+	)
 
 	c.topologyBuilder = &topology.Builder{
 		ServiceLister:        c.serviceLister,
@@ -214,6 +215,11 @@ func (c *Controller) Run(stopCh <-chan struct{}) error {
 	// Start the informers.
 	if err := c.startInformers(stopCh, 10*time.Second); err != nil {
 		return fmt.Errorf("could not start informers: %w", err)
+	}
+
+	// Load the TCP and UDP port mapper states.
+	if err := c.loadPortMapperStates(); err != nil {
+		return fmt.Errorf("could not load port mapper states: %w", err)
 	}
 
 	// Enable API readiness endpoint, informers are started and default conf is available.
@@ -284,6 +290,19 @@ func (c *Controller) startACLInformers(ctx context.Context, stopCh <-chan struct
 		if !ok {
 			return fmt.Errorf("timed out waiting for controller caches to sync: %s", t)
 		}
+	}
+
+	return nil
+}
+
+// loadPortMapperStates loads the TCP and UDP port mapper states.
+func (c *Controller) loadPortMapperStates() error {
+	if err := c.tcpStateTable.LoadState(); err != nil {
+		return fmt.Errorf("unable to load TCP state table: %w", err)
+	}
+
+	if err := c.udpStateTable.LoadState(); err != nil {
+		return fmt.Errorf("unable to load UDP state table: %w", err)
 	}
 
 	return nil
