81cb38787e03d19a1b0e0e29acf3daf034217209
kevinpollet <pollet.kevin@gmail.com>
2020-06-22 15:40:45 +0200

    review: apply @jspdown suggestions

--- pkg/cleanup/cleanup.go
+++ pkg/cleanup/cleanup.go
@@ -4,34 +4,34 @@ import (
 	"fmt"
 
 	"github.com/containous/maesh/pkg/dns"
-	"github.com/containous/maesh/pkg/k8s"
 	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 // Cleanup holds the clients for the various resource controllers.
 type Cleanup struct {
-	namespace string
-	client    k8s.Client
-	dnsClient *dns.Client
-	logger    logrus.FieldLogger
+	namespace  string
+	kubeClient kubernetes.Interface
+	dnsClient  *dns.Client
+	logger     logrus.FieldLogger
 }
 
 // NewCleanup returns an initialized cleanup object.
-func NewCleanup(logger logrus.FieldLogger, client k8s.Client, namespace string) *Cleanup {
-	dnsClient := dns.NewClient(logger, client.KubernetesClient())
+func NewCleanup(logger logrus.FieldLogger, kubeClient kubernetes.Interface, namespace string) *Cleanup {
+	dnsClient := dns.NewClient(logger, kubeClient)
 
 	return &Cleanup{
-		client:    client,
-		logger:    logger,
-		namespace: namespace,
-		dnsClient: dnsClient,
+		kubeClient: kubeClient,
+		logger:     logger,
+		namespace:  namespace,
+		dnsClient:  dnsClient,
 	}
 }
 
 // CleanShadowServices deletes all shadow services from the cluster.
 func (c *Cleanup) CleanShadowServices() error {
-	serviceList, err := c.client.KubernetesClient().CoreV1().Services(c.namespace).List(metav1.ListOptions{
+	serviceList, err := c.kubeClient.CoreV1().Services(c.namespace).List(metav1.ListOptions{
 		LabelSelector: "app=maesh,type=shadow",
 	})
 	if err != nil {
@@ -39,7 +39,7 @@ func (c *Cleanup) CleanShadowServices() error {
 	}
 
 	for _, s := range serviceList.Items {
-		if err := c.client.KubernetesClient().CoreV1().Services(s.Namespace).Delete(s.Name, &metav1.DeleteOptions{}); err != nil {
+		if err := c.kubeClient.CoreV1().Services(s.Namespace).Delete(s.Name, &metav1.DeleteOptions{}); err != nil {
 			return err
 		}
 	}
--- pkg/cleanup/cleanup_test.go
+++ pkg/cleanup/cleanup_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/containous/maesh/pkg/k8s"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -16,13 +17,13 @@ func TestCleanup_New(t *testing.T) {
 	defer cancel()
 
 	clientMock := k8s.NewClientMock(t, ctx.Done(), "mock.yaml", false)
-	log := logrus.New()
+	logger := logrus.New()
 
-	log.SetOutput(os.Stdout)
-	log.SetLevel(logrus.DebugLevel)
+	logger.SetOutput(os.Stdout)
+	logger.SetLevel(logrus.DebugLevel)
 
-	cln := NewCleanup(log, clientMock, metav1.NamespaceDefault)
-	assert.NotNil(t, cln)
+	cleanup := NewCleanup(logger, clientMock.KubernetesClient(), metav1.NamespaceDefault)
+	require.NotNil(t, cleanup)
 }
 
 func TestCleanup_CleanShadowServices(t *testing.T) {
@@ -30,24 +31,24 @@ func TestCleanup_CleanShadowServices(t *testing.T) {
 	defer cancel()
 
 	clientMock := k8s.NewClientMock(t, ctx.Done(), "mock.yaml", false)
-	log := logrus.New()
+	logger := logrus.New()
 
-	log.SetOutput(os.Stdout)
-	log.SetLevel(logrus.DebugLevel)
+	logger.SetOutput(os.Stdout)
+	logger.SetLevel(logrus.DebugLevel)
 
-	cln := NewCleanup(log, clientMock, "maesh")
-	assert.NotNil(t, cln)
+	cleanup := NewCleanup(logger, clientMock.KubernetesClient(), "maesh")
+	require.NotNil(t, cleanup)
 
-	err := cln.CleanShadowServices()
-	assert.NoError(t, err)
+	err := cleanup.CleanShadowServices()
+	require.NoError(t, err)
 
-	sl, err := clientMock.KubernetesClient().CoreV1().Services(metav1.NamespaceAll).List(metav1.ListOptions{
+	serviceList, err := clientMock.KubernetesClient().CoreV1().Services(metav1.NamespaceAll).List(metav1.ListOptions{
 		LabelSelector: "app=maesh,type=shadow",
 	})
-	assert.NoError(t, err)
-	assert.Len(t, sl.Items, 0)
+	require.NoError(t, err)
+	assert.Len(t, serviceList.Items, 0)
 
-	srv, err := clientMock.KubernetesClient().CoreV1().Services(metav1.NamespaceAll).List(metav1.ListOptions{})
-	assert.NoError(t, err)
-	assert.Len(t, srv.Items, 2)
+	serviceList, err = clientMock.KubernetesClient().CoreV1().Services(metav1.NamespaceAll).List(metav1.ListOptions{})
+	require.NoError(t, err)
+	assert.Len(t, serviceList.Items, 2)
 }
