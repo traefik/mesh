15f067adab25366ff4840b615c1139b60bc1534c
Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>
2023-10-14 18:58:47 +0200

    Fix linter issues except AddEventHandler and unused-parameter

--- cmd/configuration.go
+++ cmd/configuration.go
@@ -6,23 +6,23 @@ import (
 
 // TraefikMeshConfiguration wraps the static configuration and extra parameters.
 type TraefikMeshConfiguration struct {
-	ConfigFile       string   `description:"Configuration file to use. If specified all other flags are ignored." export:"true"`
-	KubeConfig       string   `description:"Path to a kubeconfig. Only required if out-of-cluster." export:"true"`
+	ConfigFile       string   `description:"Configuration file to use. If specified all other flags are ignored."                                          export:"true"`
+	KubeConfig       string   `description:"Path to a kubeconfig. Only required if out-of-cluster."                                                        export:"true"`
 	MasterURL        string   `description:"The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster." export:"true"`
-	LogLevel         string   `description:"The log level." export:"true"`
-	LogFormat        string   `description:"The log format." export:"true"`
-	Debug            bool     `description:"Debug mode, deprecated, use --loglevel=debug instead." export:"true"`
-	ACL              bool     `description:"Enable ACL mode." export:"true"`
-	SMI              bool     `description:"Enable SMI operation, deprecated, use --acl instead." export:"true"`
-	DefaultMode      string   `description:"Default mode for mesh services." export:"true"`
-	Namespace        string   `description:"The namespace that Traefik Mesh is installed in." export:"true"`
-	WatchNamespaces  []string `description:"Namespaces to watch." export:"true"`
-	IgnoreNamespaces []string `description:"Namespaces to ignore." export:"true"`
-	APIPort          int32    `description:"API port for the controller." export:"true"`
-	APIHost          string   `description:"API host for the controller to bind to." export:"true"`
-	LimitHTTPPort    int32    `description:"Number of HTTP ports allocated." export:"true"`
-	LimitTCPPort     int32    `description:"Number of TCP ports allocated." export:"true"`
-	LimitUDPPort     int32    `description:"Number of UDP ports allocated." export:"true"`
+	LogLevel         string   `description:"The log level."                                                                                                export:"true"`
+	LogFormat        string   `description:"The log format."                                                                                               export:"true"`
+	Debug            bool     `description:"Debug mode, deprecated, use --loglevel=debug instead."                                                         export:"true"`
+	ACL              bool     `description:"Enable ACL mode."                                                                                              export:"true"`
+	SMI              bool     `description:"Enable SMI operation, deprecated, use --acl instead."                                                          export:"true"`
+	DefaultMode      string   `description:"Default mode for mesh services."                                                                               export:"true"`
+	Namespace        string   `description:"The namespace that Traefik Mesh is installed in."                                                              export:"true"`
+	WatchNamespaces  []string `description:"Namespaces to watch."                                                                                          export:"true"`
+	IgnoreNamespaces []string `description:"Namespaces to ignore."                                                                                         export:"true"`
+	APIPort          int32    `description:"API port for the controller."                                                                                  export:"true"`
+	APIHost          string   `description:"API host for the controller to bind to."                                                                       export:"true"`
+	LimitHTTPPort    int32    `description:"Number of HTTP ports allocated."                                                                               export:"true"`
+	LimitTCPPort     int32    `description:"Number of TCP ports allocated."                                                                                export:"true"`
+	LimitUDPPort     int32    `description:"Number of UDP ports allocated."                                                                                export:"true"`
 }
 
 // NewTraefikMeshConfiguration creates a TraefikMeshConfiguration with default values.
@@ -47,16 +47,16 @@ func NewTraefikMeshConfiguration() *TraefikMeshConfiguration {
 
 // PrepareConfiguration holds the configuration to prepare the cluster.
 type PrepareConfiguration struct {
-	ConfigFile    string `description:"Configuration file to use. If specified all other flags are ignored." export:"true"`
-	KubeConfig    string `description:"Path to a kubeconfig. Only required if out-of-cluster." export:"true"`
+	ConfigFile    string `description:"Configuration file to use. If specified all other flags are ignored."                                          export:"true"`
+	KubeConfig    string `description:"Path to a kubeconfig. Only required if out-of-cluster."                                                        export:"true"`
 	MasterURL     string `description:"The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster." export:"true"`
-	LogLevel      string `description:"The log level." export:"true"`
-	LogFormat     string `description:"The log format." export:"true"`
-	Debug         bool   `description:"Debug mode, deprecated, use --loglevel=debug instead." export:"true"`
-	Namespace     string `description:"The namespace that Traefik Mesh is installed in." export:"true"`
-	ClusterDomain string `description:"Your internal K8s cluster domain." export:"true"`
-	SMI           bool   `description:"Enable SMI operation, deprecated, use --acl instead." export:"true"`
-	ACL           bool   `description:"Enable ACL mode." export:"true"`
+	LogLevel      string `description:"The log level."                                                                                                export:"true"`
+	LogFormat     string `description:"The log format."                                                                                               export:"true"`
+	Debug         bool   `description:"Debug mode, deprecated, use --loglevel=debug instead."                                                         export:"true"`
+	Namespace     string `description:"The namespace that Traefik Mesh is installed in."                                                              export:"true"`
+	ClusterDomain string `description:"Your internal K8s cluster domain."                                                                             export:"true"`
+	SMI           bool   `description:"Enable SMI operation, deprecated, use --acl instead."                                                          export:"true"`
+	ACL           bool   `description:"Enable ACL mode."                                                                                              export:"true"`
 }
 
 // NewPrepareConfiguration creates a PrepareConfiguration with default values.
@@ -74,12 +74,12 @@ func NewPrepareConfiguration() *PrepareConfiguration {
 
 // CleanupConfiguration holds the configuration for the cleanup command.
 type CleanupConfiguration struct {
-	ConfigFile string `description:"Configuration file to use. If specified all other flags are ignored." export:"true"`
-	KubeConfig string `description:"Path to a kubeconfig. Only required if out-of-cluster." export:"true"`
+	ConfigFile string `description:"Configuration file to use. If specified all other flags are ignored."                                          export:"true"`
+	KubeConfig string `description:"Path to a kubeconfig. Only required if out-of-cluster."                                                        export:"true"`
 	MasterURL  string `description:"The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster." export:"true"`
-	Namespace  string `description:"The namespace that Traefik Mesh is installed in." export:"true"`
-	LogLevel   string `description:"The log level." export:"true"`
-	LogFormat  string `description:"The log format." export:"true"`
+	Namespace  string `description:"The namespace that Traefik Mesh is installed in."                                                              export:"true"`
+	LogLevel   string `description:"The log level."                                                                                                export:"true"`
+	LogFormat  string `description:"The log format."                                                                                               export:"true"`
 }
 
 // NewCleanupConfiguration creates CleanupConfiguration.
